32e4c081d737ad5fa85c93ce67467df1516d162e
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-10-20 22:26:02 +0500

    Add working tests on comment counter

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,7 +1,6 @@
-const commentsload = document.querySelector('.commentsload');
-let counter = 0;
-
 const commentCounterFunction = () => {
+  const commentsload = document.querySelector('.commentsload');
+  let counter = 0;
   counter = commentsload.getElementsByTagName('p').length;
   return counter;
 };
