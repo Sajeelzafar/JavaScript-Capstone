4a12a2008747940ad0e38bc20ea870b76400fdb5
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-20 23:45:02 +0200

    Update fetchlike.js

--- src/modules/fetchlike.js
+++ src/modules/fetchlike.js
@@ -1,14 +1,7 @@
-const like = document.querySelector(".like");
-
 export default async function fetchlike() {
-  like.innerHTML = "";
-  await fetch(
-    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/likes`
-  )
-    .then((res) => res.json())
-    .then((output) => {
-      output.forEach((like) => {
-        like.innerHTML += `<p><span>${like.likes} ${like.item_id}</span></p>`;
-      });
-    });
-}
+    return await fetch(
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/likes`
+    )
+      .then((res) => res.json())
+      .then((output) => output);
+  }
