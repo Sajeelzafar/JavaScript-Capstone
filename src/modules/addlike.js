ebfa4c9b6be42b007a7fbf4b3fb3c073ec29a7eb
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-20 12:24:08 +0200

    Fix linter errors

--- src/modules/addlike.js
+++ src/modules/addlike.js
@@ -1,10 +1,12 @@
-const addlike = (Obj) => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/likes', 
-{
-  method: 'POST',
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
+const addlike = (Obj) => fetch(
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/likes',
+  {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(Obj),
   },
-  body: JSON.stringify(Obj),
-}).then((res) => res);
+).then((res) => res);
 
 export default addlike;
