de0e5d2c2badcaba5f388ab0725b0f98b15d2361
Selma <61165443+SelmaNdapanda@users.noreply.github.com>
2022-10-20 11:31:27 +0200

    Update addlike.js

--- src/modules/addlike.js
+++ src/modules/addlike.js
@@ -1,4 +1,5 @@
-const addlike = (Obj) => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/comments', {
+const addlike = (Obj) => fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/1RjuQJrVTvT45Mcm7X6n/likes', 
+{
   method: 'POST',
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
